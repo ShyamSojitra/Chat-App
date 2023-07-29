006609b32d12f469d69cbb1ee0e04129a6416d84
shyam <faltulal369@gmail.com>
2023-07-29 19:35:08 +0530

    chat app completed

---
+++ src/main/java/com/app/chat/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.app.chat.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private  String content;
+    private String sender;
+    private MessageType type;
+}
